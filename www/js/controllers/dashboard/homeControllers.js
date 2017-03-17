2afc7a5ec3870b1e074808a83b97576f975d3199
AJ <ajay.vjn17@gmail.com>
2017-03-17 08:10:21 -0700

    [#141445915] Change layout according to new design

--- www/js/controllers/dashboard/homeControllers.js
+++ www/js/controllers/dashboard/homeControllers.js
@@ -2,4 +2,22 @@
 
 angular.module('homeControllers', [])
   .controller('HomeTabCtrl', function ($scope, $ionicModal, $timeout) {
+    // TODO: Revove after fetching real data
+    // Site List
+    $scope.sites = [
+      {
+        name: 'Event Site 1',
+        type: 'School Playground',
+        img: 'img/no-image2.png'
+      },
+      {
+        name: 'Event Site 2',
+        type: 'Sports Arena',
+        img: 'img/no-image1.png'
+      },
+      {
+        name: 'Event Site 3',
+        type: 'Community Hall'
+      }
+    ]
   })
